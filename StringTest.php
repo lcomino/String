63c1796f4eb5e7722c48d238de310ee02a5602ec
Lucas <lcomino3d@gmail.com>
2013-03-10 22:48:17 -0300

    Adicionando classe

--- StringTest.php
+++ StringTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('String/String.php');
+
 $texto = "Teste pra ver se classe funciona! http://github.com se funcionar o link vai aparecer! http://www.fronteirasemfim.com.br";
 
 $string = new String($texto);
