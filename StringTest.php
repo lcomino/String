9f8b3b04028481eb4ce4d9b070bdacdfdb129562
Lucas <lcomino3d@gmail.com>
2013-03-10 22:47:11 -0300

    Adicionando classe

---
+++ StringTest.php
@@ -0,0 +1,7 @@
+<?php
+
+$texto = "Teste pra ver se classe funciona! http://github.com se funcionar o link vai aparecer! http://www.fronteirasemfim.com.br";
+
+$string = new String($texto);
+
+echo $string->transformUrls();
